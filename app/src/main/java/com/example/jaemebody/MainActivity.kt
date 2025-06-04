1c169bf86e85dc6412d6bee1dc8d293c1694bc6d
soneusida123 <rytn28@gmail.com>
2025-06-05 04:30:31 +0900

    Feat: 상단 상태바 숨기기 추가
    Feat: 상단 상태바 숨기기 추가

--- app/src/main/java/com/example/jaemebody/MainActivity.kt
+++ app/src/main/java/com/example/jaemebody/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jaemebody
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -42,6 +43,16 @@ class MainActivity : ComponentActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        // 앱 상단 상태바 숨기기
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+        actionBar?.hide()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {  // API 33 이상
             if (ContextCompat.checkSelfPermission(this, imagePermission)
--- app/src/main/java/com/example/jaemebody/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/example/jaemebody/ui/intro/IntroActivity.kt
@@ -3,6 +3,7 @@ package com.example.jaemebody.ui.intro
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -35,6 +36,15 @@ class IntroActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 앱 상단 상태바 숨기기
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+        actionBar?.hide()
 
         installSplashScreen()
 
